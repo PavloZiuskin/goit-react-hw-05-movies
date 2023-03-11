f0bb221e1bb0fe9f7aac285b25de108b4bb62820
PavloZiuskin <passzuskin@gmail.com>
2023-03-11 17:38:51 +0200

    add-route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "views/Home/Home";
+import Movies from "views/Movies/Movies";
+import MovieDetails from "views/MovieDetails/MovieDetails";
+import Navigation from "./Navigation/Navigation";
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+  return (<div>
+    <Routes>
+      <Route path='/' element={<Navigation/> }>
+        <Route index element={<Home/> } />
+        <Route path='/movies' element={<Movies/> }/>
+        <Route path='/movies/:id' element={<MovieDetails/> }/>
+      </Route>
+    </Routes>
     </div>
   );
 };
